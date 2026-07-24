4472f8a2975330839223962b1466517e531bcf6a
Abhay Singh <abhay.25b01011377@abes.ac.in>
2026-07-24 15:09:33 +0530

    eventemitter

---
+++ lab1/Eventemitter.js
@@ -0,0 +1,32 @@
+import{ EventEmitter } from'node:events';
+const login=(name)=>{
+    console.log(`${name} logged in`);
+};
+ const start=()=>{
+    console.log("system start");
+};
+
+ const working =(name)=>{
+    console.log(`${name} add items to cart`);
+ };
+ const checkout =(name)=>{
+    console.log(`${name} logged out`)
+ };
+const task= new EventEmitter();
+
+task.once("greet",start);
+task.on("greet",login);
+task.on("greet",working);
+task.on("greet",checkout);
+task.once("exit",()=>{
+    console.log("system shutting down")
+});
+task.emit("greet", "Abhay Singh");
+task.emit("greet", "karishma srivastava");
+task.off("greet",working)
+task.emit("greet","janhvi dwivedi");
+task.emit("exit","Abhay singh");
+
+
+ 
+ 
---
+++ lab1/fun.js
@@ -0,0 +1,10 @@
+console.log("Abhay Singh")
+function sum(a,b){
+    return a+b;
+}
+const add=(a,b)=>{
+    return a+b;
+};
+const add2=(a,b)=>a+b;
+
+//backend nodejs, mongodb ,springboot, fast api ,django,php, 
