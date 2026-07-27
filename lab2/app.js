3ae3267b7a66b19fe65541c1a1824ecae7b15976
Abhay Singh <abhay.25b01011377@abes.ac.in>
2026-07-27 22:42:33 +0530

    app.js

---
+++ lab2/app.js
@@ -0,0 +1,43 @@
+//manual way to print
+
+// console.log("1");
+// console.log("2");
+// console.log("3");
+// console.log("4");
+// console.log("5");
+// console.log("6");
+
+//loops using
+
+// for(let i=1;i<=5;i++){
+//     console.log(i);
+// }
+// for(let k=5;k>=1;k=k-2){
+//     console.log(k);
+// }
+// for(let j=1;j<=15;j=j+2){
+//     console.log(j);
+// }
+// for (let l=2;l<=10;l=l+2){
+//     console.log(l);
+// }
+//  let n= prompt("write your number"); 
+//  n=parseInt(n);
+
+// for(let m=n;m<=n*10;m=m+n){
+//     console.log(m);
+// }
+
+
+// for(i=1;i<=3;i++){
+//     console.log(`outer loop${i}`)
+//     for(j=1;j<=3;j++){
+//         console.log(j);
+//     }
+
+// While loop
+ let j=2;
+ while(j<=10){
+    console.log(j);
+    j=j+2;
+ }
