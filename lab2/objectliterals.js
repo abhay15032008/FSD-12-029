d691ce41cc23bf2a4f732f43a714ef31233423ff
Abhay Singh <abhay.25b01011377@abes.ac.in>
2026-08-05 21:15:05 +0530

    objectliterals

--- lab2/objectliterals.js
+++ lab2/objectliterals.js
@@ -1,114 +1,141 @@
-let delhi={
-    latitude :'28.97874 N',
-    longitude :"77.56483 E"
+// let delhi={
+//     latitude :'28.97874 N',
+//     longitude :"77.56483 E"
 
-};
-const student={ // object
-    name:"Abhay",// this is key value pairs 
-    //and these are properties also
-    age: 20,
-    marks: 94.4,
-    city : 'delhi'
-};
-const item={
-    price: 100.99,
-    discount: 15,
-    colors: ["red", "pink"],
-    stock: 100,
-};
-let post={
-    username: "physicswallah",
-    content: " related to education",
-    likes: 100000,
-    reposts: 15000,
-    tags: ["@education minister", "@NCERT"],
-};
-// how to get values of respective keys
-//write the object name and square brackets then dobule quote  value name u can access the values
-// student["marks"]
-// 94.4
-const obj={
-    1:"a",//ye keyword nhi hai js mein oblect literals mein ye string kehlta hai
-    2:'b',
-    true:'c',
-    null:"d",
-    undefined:"e"
+// };
+// const student={ // object
+//     name:"Abhay",// this is key value pairs 
+//     //and these are properties also
+//     age: 20,
+//     marks: 94.4,
+//     city : 'delhi'
+// };
+// const item={
+//     price: 100.99,
+//     discount: 15,
+//     colors: ["red", "pink"],
+//     stock: 100,
+// };
+// let post={
+//     username: "physicswallah",
+//     content: " related to education",
+//     likes: 100000,
+//     reposts: 15000,
+//     tags: ["@education minister", "@NCERT"],
+// };
+// // how to get values of respective keys
+// //write the object name and square brackets then dobule quote  value name u can access the values
+// // student["marks"]
+// // 94.4
+// const obj={
+//     1:"a",//ye keyword nhi hai js mein oblect literals mein ye string kehlta hai
+//     2:'b',
+//     true:'c',
+//     null:"d",
+//     undefined:"e"
 
 
-};
-//Now we have to do Add/update in object literals
+// };
+// //Now we have to do Add/update in object literals
 
-const student1={
-    name:'abhay',
-    age:'20',
-    marks:92,
-    city:'katra'
+// const student1={
+//     name:'abhay',
+//     age:'20',
+//     marks:92,
+//     city:'katra'
 
-};
+// };
 
-//Nested object literals
+// //Nested object literals
 
- const data={
-    abhay:{
-        grade:94.4,
-        city:'Fatehpur'
-    },
-    kanak:{
-        grade:82.4,
-        city:'pathankot',
-    },
-    palak:{
-        grade:85.3,
-        city:'kanpur'
-    },
- };
- const classinfo=[
-    {
-        name:'jay',
-        class:'UKG',
-    },
-    {
-        name:'shaurya',
-        class:1,
-    },
-    {
-        name:'ananjay',
-        class:4
+//  const data={
+//     abhay:{
+//         grade:94.4,
+//         city:'Fatehpur'
+//     },
+//     kanak:{
+//         grade:82.4,
+//         city:'pathankot',
+//     },
+//     palak:{
+//         grade:85.3,
+//         city:'kanpur'
+//     },
+//  };
+//  const classinfo=[
+//     {
+//         name:'jay',
+//         class:'UKG',
+//     },
+//     {
+//         name:'shaurya',
+//         class:1,
+//     },
+//     {
+//         name:'ananjay',
+//         class:4
 
-    },
+//     },
 
- ];
+//  ];
 
- //Maths objects
+//  //Maths objects
 
-Math.PI
-Math.E
- //methods
- Math.abs // absolute
- Math.pow(a, b) //power of a
- Math.floor(n)// whole value nearest smalest integer
- Math.ceil(n) // whole value nearest larger value
- Math.random() // random value b/w 0-1
+// Math.PI
+// Math.E
+//  //methods
+//  Math.abs // absolute
+//  Math.pow(a, b) //power of a
+//  Math.floor(n)// whole value nearest smalest integer
+//  Math.ceil(n) // whole value nearest larger value
+//  Math.random() // random value b/w 0-1
  
 
 
 //  interesting game
-const max=prompt('enter the  max.number to guess');
-const random=Math.floor(Math.random()*max)+1;
-let guess=prompt('guess the number');
-while(true){
-    if(guess=='quit'){
-        console.log('user quit')
-        break;
-    }
-    if (guess==random){
-        console.log('HINT : your guess is true randon no. was',random);
-        break;
+// const max=prompt('enter the  max.number to guess');
+// const random=Math.floor(Math.random()*max)+1;
+// let guess=prompt('guess the number');
+// while(true){
+//     if(guess=='quit'){
+//         console.log('user quit')
+//         break;
+//     }
+//     if (guess==random){
+//         console.log('HINT : your guess is true randon no. was',random);
+//         break;
 
-    }else if(guess<random){
-        guess= prompt('HINT : your guess was small ,please try again');
-    }else{
-        guess=prompt('your number was to large , try again')
-    }
+//     }else if(guess<random){
+//         guess= prompt('HINT : your guess was small ,please try again');
+//     }else{
+//         guess=prompt('your number was to large , try again')
+//     }
     
-}
+// }
+
+//Assignment questions
+// const dice=prompt('enter the dice number');
+// const random=Math.floor(Math.random()*dice)+1;
+// let guess=prompt("guess the number");
+// while(true){
+    
+//     if(guess==="quit"){
+//         console.log('you had quit')
+//         break;
+//     }
+//     if(guess==random){
+//         console.log('you guess is true')
+//         break;
+//     } else{
+//         guess=prompt('try again')
+//     }
+
+    //}
+
+// object creation
+
+// let car={
+//     name:"hyundai",
+//     model:"seriesXZ",
+//     color:"White"
+// };
