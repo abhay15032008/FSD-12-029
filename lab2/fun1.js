6051efbde23315b20634f2de035d17f4230af6fb
Abhay Singh <abhay.25b01011377@abes.ac.in>
2026-08-13 11:51:26 +0530

    read/append/data

--- lab2/fun1.js
+++ lab2/fun1.js
@@ -1,3 +1,7 @@
 
-import { writeFile } from "fs/promises";
-await writeFile("hello.txt", "JS is easy");
+import { writeFile , appendFile,readFile } from "fs/promises";
+//await writeFile("hello.txt", "JS is easy");
+// await appendFile("hello.txt", " \n is much easy than others ");
+ await appendFile("hello.txt", " \n Abhay singh 😍 ");
+const content=await readFile("hello.txt" , "utf-8");
+console.log(content);
